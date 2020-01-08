b27f97fecf13bb7f2a9ff6877a234921f02152eb
Utkarsh Singh <utkarsh0411@gmail.com>
2020-01-08 17:49:02 +0530

    "views and partials added"

--- v1/app.js
+++ v1/app.js
@@ -1,13 +1,18 @@
-var express                 = require('express');
-var bodyParser              = require('body-parser');
-var mongoose                = require('mongoose');
-var request                 = require('request');
-var passport                = require('passport');
-var passportLocal           = require('passport-local');
-var passportLocalMongoose   = require('passport-local-mongoose');
-var methodOverride          = require('method-override');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var request = require('request');
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
+var methodOverride = require('method-override');
 
 var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("landing");
+});
 
 app.listen(3000, function () {
     console.log("Server Listening...")
